77d214aba9847dd8f8c495ef3f0877099753ccf0
Batheesha222 <batheesha.dilmeen@gmail.com>
2026-04-01 15:19:22 +0530

    add flashcard routes to note service

--- hive/services/note-service/src/routes/noteRoutes.js
+++ hive/services/note-service/src/routes/noteRoutes.js
@@ -4,14 +4,25 @@ const {
   createNote,
   deleteNote,
   updateNote,
+  getFlashCardDecks,
+  createFlashCardDeck,
+  updateFlashCardDeck,
+  deleteFlashCardDeck,
 } = require("../controllers/noteController"); 
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Note routes
 router.get('/', authMiddleware, getNotes);
 router.post('/create', authMiddleware, createNote);
 router.delete('/delete/:id', authMiddleware, deleteNote);
 router.put('/update/:id', authMiddleware, updateNote);
+
+// Flashcard routes
+router.get('/flashcards', authMiddleware, getFlashCardDecks);
+router.post('/flashcards', authMiddleware, createFlashCardDeck);
+router.put('/flashcards/:id', authMiddleware, updateFlashCardDeck);
+router.delete('/flashcards/:id', authMiddleware, deleteFlashCardDeck);
 
 module.exports = router;
